5f41a62affffe5592ad3ed67fba181c17e717c16
Manikanta <mani@gmail.com>
2018-09-28 11:19:41 +0530

    ui is adjusted

--- src/components/Square/index.js
+++ src/components/Square/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { MainContainer } from './styledComponents'
+import { PLAYER_O, PLAYER_X } from '../../constants/AppConstants'
+import { MainContainer, CircleShape, CrossLine } from './styledComponents'
 
 class Square extends Component {
   onSquareSelect = () => {
@@ -8,7 +9,18 @@ class Square extends Component {
   }
   render() {
     const { value } = this.props
-    return <MainContainer onClick={this.onSquareSelect}>{value}</MainContainer>
+    return (
+      <MainContainer onClick={this.onSquareSelect}>
+        {value === PLAYER_O ? (
+          <CircleShape />
+        ) : value === PLAYER_X ? (
+          <div>
+            <CrossLine rotateLeft />
+            <CrossLine rotateRight />
+          </div>
+        ) : null}
+      </MainContainer>
+    )
   }
 }
 
--- src/components/Square/styledComponents.js
+++ src/components/Square/styledComponents.js
@@ -1,10 +1,39 @@
 import styled from 'react-emotion'
 
 export const MainContainer = styled(`div`)`
-  border: solid 1px black;
-  height: 44px;
+  height: 55px;
   cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
+
+  background-color: #9a8297;
+  border-radius: 6px;
+  transition: background-color 0.3s;
+  position: relative;
+
+  &:hover {
+    background-color: #d0782a;
+  }
+`
+export const CircleShape = styled(`div`)`
+  position: absolute;
+  border-radius: 50%;
+  border: solid 6px white;
+  width: 25px;
+  height: 25px;
+`
+export const CrossLine = styled(`div`)`
+  width: 6px;
+  height: 50px;
+  background-color: white;
+  display: block;
+  position: absolute;
+  border-radius: 3px;
+  transform: ${props =>
+    props.rotateLeft
+      ? 'translate(-50%, -50%) rotate(-45deg)'
+      : props.rotateRight
+        ? 'translate(-50%, -50%) rotate(45deg)'
+        : 'none'};
 `
